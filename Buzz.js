542834826a8f85bbd36fba2d30250c67f514d5e4
Alex Montas <Alexmontasmusic@gmail.com>
2014-04-28 21:03:08 -0400

    Fizzbuzz is working

--- Buzz.js
+++ Buzz.js
@@ -1,16 +1,37 @@
-	$(document).ready(function() {
-	//assigning the click event to the buttom
-	$('#addButton').click(function(){
+$(document).ready(function() {
+	
+	
+var get_Value = function (i){ 
+					   if (i % 3 === 0 & i % 5 ===0)
+					  {
+					      return "FizzBuzz";
+					  }
+					   else if (i % 3 === 0)
+					  {
+					      return "Fizz";
+					  }
+					  else if (i % 5 === 0)
+					  {
+					      return "Buzz";
+					  }  
+					  else
+					  {
+					    return i;
+					  }
+			}	
+}
 
-		var listItem = $('#listItem');
-		$('#shoppingList ul').append('<li>' + listItem.val() + "   <span class='close'> X </span>   " + '</li>');
-		listItem.val('').focus();
+
+
+$('.getNumber').click(function(){
+		var numInput = $('#numInput').val();
+		var value = get_Value(numInput);
+		$('.Buzz ul').append('<li>' + get_Value(i) + '</li>');
+		numInput.val('').focus();
 
 	});
 
 
-	$('#shoppingList').on("click", "li:contains(X)", function () { 
-    $(this).remove();
 });
 
 
